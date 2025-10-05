a1c92e1c4144812bf0b102bcbf68a29b9c0c5e94
JAM4youAlways <jmckean@thecollegefamilyplan.com>
2025-10-05 17:18:57 -0400

    Add files via upload

---
+++ config.js.js
@@ -0,0 +1,26 @@
+const CONFIG = {
+  SHEET_JSON_URL: "https://script.google.com/macros/s/AKfycbzDQg7kh_Vl5xvn6LGTDC5WnrkT4XpjLsrbUOSzg-srYNT41fQz45aqdn9Bbz1uQd18sg/exec",
+  SHEET_UPDATE_URL: "https://script.google.com/macros/s/AKfycbzDQg7kh_Vl5xvn6LGTDC5WnrkT4XpjLsrbUOSzg-srYNT41fQz45aqdn9Bbz1uQd18sg/exec",
+  MISSIONS: [
+    "Complete assessments",
+    "Create a Binder or Folder",
+    "Explore: Interests and Careers",
+    "Read and Build Vocabulary 1",
+    "Strengthen your future college application",
+    "Get Involved at School",
+    "Read and Build Vocabulary 2",
+    "Volunteer and Give Back",
+    "Build Your Resume",
+    "Plan for Summer Opportunities",
+    "Passion Project 1: Learn",
+    "Passion Project 2: Brainstorm",
+    "Passion Project 3: Choose",
+    "Stay Informed",
+    "Read and Build Vocabulary 3",
+    "PSAT Prep",
+    "AP Prep",
+    "Organize Study Space",
+    "Create Personal Action Plan",
+    "Add your counselor to contacts"
+  ]
+};
