9b34b75cfcd8f1b6bb9331f6bec816610f06c731
JAM4youAlways <jmckean@thecollegefamilyplan.com>
2025-10-05 22:52:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 
 const CONFIG = {
   // Use the same URL for both JSON fetch (doGet) and update (doPost)
-  SHEET_JSON_URL: "https://script.google.com/macros/s/AKfycbxZFfFPpb0huSUffxPj9Wzv_E54Lj8kYHDGWE3MsSUpu6CDIQGFTJFInMU-eRVaDZZBcw/exec",
-  SHEET_UPDATE_URL: "https://script.google.com/macros/s/AKfycbxZFfFPpb0huSUffxPj9Wzv_E54Lj8kYHDGWE3MsSUpu6CDIQGFTJFInMU-eRVaDZZBcw/exec",
+  SHEET_JSON_URL: "https://script.google.com/macros/s/AKfycby93bZ35KQcP-9CFoewp4_qYv_fEns6GTtBKMSNXVgXk1V0YTXqEQ4qT4kWNkOmJNf0aw/exec",
+  SHEET_UPDATE_URL: "https://script.google.com/macros/s/AKfycby93bZ35KQcP-9CFoewp4_qYv_fEns6GTtBKMSNXVgXk1V0YTXqEQ4qT4kWNkOmJNf0aw/exec",
 
   // List your missions exactly in the same order they appear in your sheet
   MISSIONS: [
